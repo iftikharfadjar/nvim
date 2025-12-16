506f48728baa8483bccaf5a27d25c527aefd65a1
iftikharfadjar <42471332+iftikharfadjar@users.noreply.github.com>
2025-12-16 19:02:05 +0700

    Update harpoon.lua

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -35,5 +35,11 @@ return {
             { desc = "Open harpoon window" })
         vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
         vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+                vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+
     end
 }
