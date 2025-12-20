8dbf64c8dd498ceb42e98e01b474ad52497d9782
fadjar <fadjar.iftikhar@gmail.com>
2025-12-20 10:56:42 +0700

    add number neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,15 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
+    opts = {
+        event_handlers = {
+        {
+            event = "neo_tree_buffer_enter",
+            handler = function()
+                vim.opt_local.relativenumber = true
+            end
+                }
+            }
+        }
   }
 }
